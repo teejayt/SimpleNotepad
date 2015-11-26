a5e707b59762ca055247e57d0437cb2cefd198b0
teejayt <tolanijayt4ril@yahoo.com>
2015-11-26 11:29:35 +0000

    Designed the Interface

--- SimpleNotepadUnitTestProject/UnitTest1.cs
+++ SimpleNotepadUnitTestProject/UnitTest1.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
+/// Author: Tolani Jaiye-Tikolo
+/// No Gauranteee or waranty given
+/// You allowed to fork the project on GitHuB but don't remove the name
+/// 
 namespace SimpleNotepadUnitTestProject
 {
     [TestClass]
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Test_OpenButtonOpensAdialog()
         {
         }
     }
