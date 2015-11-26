f8552ab324eb16b8e4ede66b884db9723e2a3cc3
teejayt <tolanijayt4ril@yahoo.com>
2015-11-26 12:48:35 +0000

    Added a title to change the titlebar of the form and also added PropertyChangeEvents

--- SimpleNotepadApp/ViewModel.cs
+++ SimpleNotepadApp/ViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,22 @@ namespace SimpleNotepadApp
 {
     public class ViewModel
     {
+        private string _title;
+
+        public string title
+        {
+            get { return _title; }
+            set { _title = value; }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
     }
 }
